73074133b2f72e496df53b553a228f31d48cec25
Aliaksandr Kulik <inter_net@tut.by>
2021-01-02 03:32:32 +0300

    fix: some fixes

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,10 @@ import '../../node_modules/reveal.js-appearance/plugin/appearance/appearance.css
 import '../css/style.css';
 import '../css/style.scss';
 import '../../node_modules/reveal.js/plugin/highlight/zenburn.css';
-import Reveal from '../../node_modules/reveal.js/dist/reveal';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
 import Appearance from 'reveal.js-appearance/plugin/appearance/appearance.esm';
+import Reveal from '../../node_modules/reveal.js/dist/reveal';
 
 const deck = new Reveal({
   plugins: [Markdown, RevealHighlight, Appearance],
@@ -20,4 +20,5 @@ const deck = new Reveal({
     delay: 300,
   },
 });
+
 deck.initialize();
